20ec4bd28348cf4e574b896e6a694f0449ef99f0
Dmitriy Demenko <DmitriyDem@MacBook-Air-Dmitrij.local>
2023-03-16 02:31:22 +0300

    сделал бургер у меню на мобильной версии

---
+++ source/js/js.js
@@ -0,0 +1,14 @@
+var navMain = document.querySelector('.page-header__nav');
+var navToggle = document.querySelector('.page-header__toggler');
+
+navMain.classList.add('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('page-header__nav--closed')) {
+        navMain.classList.add('page-header__nav--opened');
+        navMain.classList.remove('page-header__nav--closed');
+    } else {
+        navMain.classList.add('page-header__nav--closed');
+        navMain.classList.remove('page-header__nav--opened');
+    }
+});
